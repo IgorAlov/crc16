16a14b389cb8c7f208182a0d0da649c7c1adf834
Igor Alov <94057155+IgorAlov@users.noreply.github.com>
2022-11-03 22:50:33 +0300

    Create crc16.php

---
+++ crc16.php
@@ -0,0 +1,10 @@
+function crc16($data) //Implementation of the CRC-16/CCITT-FALSE
+{  
+   $crc=0xFFFF;
+   for ($i = 0; $ i < strlen($data); $i++) {
+     	$x = (($crc>>8) ^ ord($data[$i])) & 0xFF;
+     	$x ^= $x>>4;
+     	$crc = (($crc<<8) ^ ($x<<12) ^ ($x<<5) ^$x) & 0xFFFF;
+   }
+	return $crc;
+}
